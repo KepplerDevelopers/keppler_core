410f50dae2e5e952590d5b706a6b52953f58118d
Gabriel <gabriel@softwarecriollo.com>
2015-07-06 15:21:43 -0430

    update devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+# disabled field_with_error
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
