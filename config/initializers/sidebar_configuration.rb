22d7d1489f276405feeb861f5b89603f22b65d3e
#Inyxtech <inyxtech@gmail.com>
2015-07-14 15:58:44 -0430

    Update sidebar_configuration.rb

--- config/initializers/sidebar_configuration.rb
+++ config/initializers/sidebar_configuration.rb
@@ -2,6 +2,7 @@ require "#{Rails.root}/lib/sidebar_configuration.rb"
 
 SidebarConfiguration.setup do |config|
 	
-  config.keppler_engines =  Rails::Engine::subclasses.map { |e| e.parent if e.parent.constants.include? :ROUTES_TREE_SIDEBAR or e.parent.constants.include? :ROUTE_SIDEBAR } .compact
+  config.keppler_engines =  Rails::Engine::subclasses.map { |e| e.parent if e.parent.constants.include? :ROUTE_SIDEBAR } .compact
   
 end
+
