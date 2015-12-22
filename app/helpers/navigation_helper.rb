d26d5f36135c6ba45ac518c2ed54607326dbbc68
Gabriel Morillo <gbrlmrllo@gmail.com>
2015-12-21 20:59:12 -0430

    update navigation

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,42 +1,7 @@
 module NavigationHelper
 
-	def current_navigation?(*args)
-		args.include? current_path.camelize	
+	def navigation
+		
 	end
-
-	def tab(name, icon, link, current)	
-		link_to link, class: "collapsible-header #{'current' if current }" do
-    	content_tag(:i, icon, class: "mi md-18")+name
-    end
-	end
-
-	def header_tab(name, icon, current) 
-		content_tag :div, class: "collapsable collapsible-header #{'active current' if current }" do
-			content_tag(:i, icon, class: "mi md-18")+name
-		end
-	end
-
-	def body_tab(&block)
-		content_tag :div, class: "collapsible-body" do
-			content_tag(:ul, capture(&block), class: "pageslide-sub-menu")
-		end
-	end
-
-	def link_tab(name, link, current)
-		content_tag :li do
-			link_to name, link, class: "#{'current' if current }"
-		end
-	end
-
-	private
-
-	def current_path
-		path = controller_path.split("/")
-		case path.size
-			when 1
-				path.first
-			when 2
-				path.first.camelize
-		end
-	end
+	
 end
--- config/initializers/keppler_configuration.rb
+++ config/initializers/keppler_configuration.rb
@@ -3,4 +3,5 @@ require "#{Rails.root}/lib/keppler_configuration.rb"
 KepplerConfiguration.setup do |config|
 	config.skip_module_devise = [:registrations] # acepta array, permite escoger los modulos de devise, ejemplo: [:registrations, :confirmations]
 	config.default_per_page = 25 #acepta integer, permite escoger la cantidad de items por pagina en el index
+	config.visible_models =  YAML.load_file("#{Rails.root}/config/menu.yml").values.each { |hash| hash.symbolize_keys! }
 end
