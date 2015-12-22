b73372ff1d4b024bdb28ecd59c82fe4a1724ae3a
Gabriel Morillo <gbrlmrllo@gmail.com>
2015-12-22 12:09:34 -0430

    update

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module InyxmaterRails
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('plugins', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :es
 
     config.active_record.raise_in_transactional_callbacks = true
 
