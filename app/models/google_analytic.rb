0a42119384f40bf81897ab8956ddb7de842ab569
Gabriel Morillo <gbrlmrllo@gmail.com>
2016-01-27 21:57:09 -0430

    update

--- app/models/google_analytic.rb
+++ app/models/google_analytic.rb
@@ -1,5 +1,5 @@
 class GoogleAnalytic < ActiveRecord::Base
 	belongs_to :setting
 	has_many :properties
-	accepts_nested_attributes_for :properties, reject_if: :all_blank, allow_destroy: true
+	accepts_nested_attributes_for :properties, allow_destroy: true
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,8 @@
 class Property < ActiveRecord::Base
 	belongs_to :google_analytic
+	validates_presence_of :name
+
+	def code_random
+		SecureRandom.hex(4)
+	end
 end
