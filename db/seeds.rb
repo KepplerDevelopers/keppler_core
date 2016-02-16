d03a5634ecaeb0d61201cdef85ffd3b3fcfd9d8d
Gabriel Morillo <gbrlmrllo@gmail.com>
2016-02-16 09:58:07 -0430

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+# This file should contain all the record creation needed to seed
+# the database with its default values.
+# The data can then be loaded with the rake db:seed
+# (or created alongside the db with db:setup).
 #
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#user = CreateAdminService.new.call
-#puts 'CREATED ADMIN USER: ' << user.email
+# user = CreateAdminService.new.call
+# puts 'CREATED ADMIN USER: ' << user.email
 
-[:admin, :client].each do |name|
-	Role.create name: name
-	puts "#{name} has been created"
+[:director, :project_manager, :developer,
+ :designer, :supporting, :comunnity_manager, :billing].each do |name|
+  Role.create name: name
+  puts '#{name} has been created'
 end
 
-#create user default
-User.create name: "Admin", email: "admin@keppler.com", password: "12345678", password_confirmation: "12345678", role_ids: "1"
-puts "admin@keppler.com has been created"
+# Create user default
+User.create(
+  name: 'Admin', email: 'admin@keppler.com', password: '12345678',
+  password_confirmation: '12345678', role_ids: '1'
+)
 
-#create setting deafult
-setting = Setting.new(name: "Keppler Admin", description: "Welcome to Keppler Admin", smtp_setting_attributes: { address: "test", port: "25", domain_name: "keppler.com", email: "info@keppler.com", password: "12345678" }, google_analytics_setting_attributes: { ga_account_id: "60688852", ga_tracking_id: "UA-60688852-1", ga_status: true })
+puts 'admin@keppler.com has been created'
+
+# Create setting deafult
+setting = Setting.new(
+  name: 'Keppler Admin', description: 'Welcome to Keppler Admin',
+  smtp_setting_attributes: {
+    address: 'test', port: '25', domain_name: 'keppler.com',
+    email: 'info@keppler.com', password: '12345678'
+  },
+  google_analytics_setting_attributes: {
+    ga_account_id: '60688852',
+    ga_tracking_id: 'UA-60688852-1',
+    ga_status: true
+  }
+)
 setting.social_account = SocialAccount.new
-setting.appearance = Appearance.new(theme_name: "keppler")
+setting.appearance = Appearance.new(theme_name: 'keppler')
 setting.save
-puts "Setting default has been created"
+puts 'Setting default has been created'
