42838a13e292aeb2776905459a6bfdcddade102f
Gabriel Morillo <gbrlmrllo@gmail.com>
2016-02-20 23:28:56 -0430

    update

--- lib/generators/keppler_scaffold/templates/models/model.rb
+++ lib/generators/keppler_scaffold/templates/models/model.rb
@@ -1,40 +1,21 @@
-require 'elasticsearch/model'
-
 # <%= class_name %> Model
 <% module_namespacing do -%>
 class <%= class_name %> < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-  include PublicActivity::Model
-  tracked owner: ->(controller, _) { controller && controller.current_user }
-
-  after_commit on: [:update] do
-    __elasticsearch__.index_document
-  end
-
-  def self.searching(query)
-    if query
-      search(query(query)).records.order(id: :desc)
-    else
-      order(id: :desc)
-    end
-  end
+  include ElasticSearchable
+  include ActivityHistory
 
   def self.query(query)
     { query: { multi_match:  {
       query: query,
-      fields: [], operator: :and }
+      fields: [<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>], operator: :and }
     }, sort: { id: 'desc' }, size: count }
   end
 
   # Build index elasticsearch
-  def as_indexed_json(_options={})
-    {
-      id: id.to_s,
-      <%- attributes.each do |attribute| -%>
-      <%= attribute.name%>: <%= attribute.name%>.to_s,
-      <%- end -%>
-    }.as_json
+  def as_indexed_json(_options = {})
+    as_json(
+      only: [<%= attributes_names.map { |name| ":#{name}" }.join(', ') %>]
+    )
   end
 end
 <% end -%>
