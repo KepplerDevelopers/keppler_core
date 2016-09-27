d5c36a7bde0ac81fbd525f06a1e18e0574211350
Luis Perez <luis.prz7@gmail.com>
2016-09-27 10:44:07 -0400

    Update app_controller.rb

--- app/controllers/app/app_controller.rb
+++ app/controllers/app/app_controller.rb
@@ -5,10 +5,10 @@ module App
     before_filter :set_metas
 
     def set_metas
-      setting = Setting.first
-      @title = setting.name
-      @description = setting.description
-      @favicon = setting.favicon
+      @setting = Setting.first
+      @title = @setting.name
+      @description = @setting.description
+      @favicon = @setting.favicon
       @meta = MetaTag.get_by_url(request.url)
       @google_adword = GoogleAdword.get_by_url(request.url)
     end
