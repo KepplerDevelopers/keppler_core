c5a22bc8417975cbcfd5ec084719923cc4615a6b
anyelopetit <anyelopetit@gmail.com>
2017-10-09 19:37:25 -0400

    added basic tooltips in index button icons

--- lib/generators/keppler_scaffold/keppler_scaffold_generator.rb
+++ lib/generators/keppler_scaffold/keppler_scaffold_generator.rb
@@ -65,13 +65,16 @@ module Rails
         end
       end
 
-      def add_config_xls
-        inject_into_file(
-          'config/initializers/mime_types.rb',
-          str_xls,
-          after: '# ActiveRecords to save as .xls'
-        )
-      end
+      # Se usa para configurar la exportación del ActiveRecords a .xls,
+      # pero da problemas al borrar el KepplerScaffold
+      
+      # def add_config_xls
+      #   inject_into_file(
+      #     'config/initializers/mime_types.rb',
+      #     str_xls,
+      #     after: '# ActiveRecords to save as .xls'
+      #   )
+      # end
 
       def create_controller_files
         template(
