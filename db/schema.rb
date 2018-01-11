b3989adfff69e2a480621647ad04bc6debed8934
Enmanuel <enmachs@gmail.com>
2018-01-11 13:13:18 -0400

    Delete all functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603145521) do
+ActiveRecord::Schema.define(version: 20171225023933) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20170603145521) do
 
   create_table "customizes", force: :cascade do |t|
     t.string   "file",       limit: 255
-    t.boolean  "installed"
+    t.boolean  "installed",  limit: 1
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
@@ -57,18 +57,10 @@ ActiveRecord::Schema.define(version: 20170603145521) do
   create_table "google_analytics_settings", force: :cascade do |t|
     t.string   "ga_account_id",  limit: 255
     t.string   "ga_tracking_id", limit: 255
-    t.boolean  "ga_status"
+    t.boolean  "ga_status",      limit: 1
     t.integer  "setting_id",     limit: 4
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-  end
-
-  create_table "google_analytics_tracks", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "tracking_id", limit: 255
-    t.string   "url",         limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
   end
 
   create_table "meta_tags", force: :cascade do |t|
@@ -91,6 +83,21 @@ ActiveRecord::Schema.define(version: 20170603145521) do
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
   add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
+  create_table "scaffolds", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "fields",     limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "scripts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.text     "script",     limit: 65535
+    t.string   "url",        limit: 255
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
   create_table "settings", force: :cascade do |t|
     t.string   "name",        limit: 255
     t.string   "description", limit: 255
@@ -104,14 +111,14 @@ ActiveRecord::Schema.define(version: 20170603145521) do
   end
 
   create_table "smtp_settings", force: :cascade do |t|
-    t.string   "address",     limit: 255
-    t.string   "port",        limit: 255
-    t.string   "domain_name", limit: 255
-    t.string   "email",       limit: 255
-    t.string   "password",    limit: 255
-    t.integer  "setting_id",  limit: 4
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "server_address", limit: 255
+    t.string   "port",           limit: 255
+    t.string   "domain_name",    limit: 255
+    t.string   "email",          limit: 255
+    t.string   "password",       limit: 255
+    t.integer  "setting_id",     limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "social_accounts", force: :cascade do |t|
