97c123d597e5a441c36716de8d3f528f8acd074c
anyelopetit <anyelopetit@gmail.com>
2018-01-13 12:58:17 -0400

    added user and dashboard options to sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
   include PublicActivity::StoreController
   include AdminHelper
 
-  rescue_from Faraday::ConnectionFailed do |error|
-    redirect_to main_app.admin_users_path, notice: "Sin conexión a internet"
-  end
+  # rescue_from Faraday::ConnectionFailed do |error|
+  #   redirect_to main_app.admin_users_path, notice: "Sin conexión a internet"
+  # end
 
   rescue_from CanCan::AccessDenied do |exception|
     exception.default_message =
