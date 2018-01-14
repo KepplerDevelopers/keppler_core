aee7f7ffc12a1c15c698774a77d80162c5bd9244
pdrowr <pdrowr@gmail.com>
2018-01-14 15:01:15 -0300

    Modified customize module

--- lib/generators/keppler_scaffold/keppler_scaffold_generator.rb
+++ lib/generators/keppler_scaffold/keppler_scaffold_generator.rb
@@ -159,7 +159,7 @@ module Rails
       end
 
       def str_route
-        "resources :#{controller_file_name} do\n    get '(page/:page)', action: :index, on: :collection, as: ''\n    get '/clone', action: 'clone'\n    post '/sort', action: 'sort'\n    delete(\n      action: :destroy_multiple,\n      on: :collection,\n      as: :destroy_multiple\n    )\n  end\n"
+        "resources :#{controller_file_name} do\n    get '(page/:page)', action: :index, on: :collection, as: ''\n    get '/clone', action: 'clone'\n    post '/import', action: 'import', as: :import\n    delete(\n      action: :destroy_multiple,\n      on: :collection,\n      as: :destroy_multiple\n    )\n  end\n"
       end
 
       def str_menu
