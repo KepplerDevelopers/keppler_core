cd7633b63f57ae779d16fda0c3e13569748cc979
enmachs <enmachs@gmail.com>
2018-01-26 10:48:19 -0400

    Terminado casi - Controlador scaffold

--- lib/generators/keppler_scaffold/templates/controllers/controller.rb
+++ lib/generators/keppler_scaffold/templates/controllers/controller.rb
@@ -14,12 +14,14 @@ module Admin
       <%= plural_table_name %> = @q.result(distinct: true)
       @objects = <%= plural_table_name %>.page(@current_page).order(position: :desc)
       @total = <%= plural_table_name %>.size
+      @<%= plural_table_name %> = <%= class_name %>.order(:position)
       if !@objects.first_page? && @objects.size.zero?
         redirect_to <%= plural_table_name %>_path(page: @current_page.to_i.pred, search: @query)
       end
       respond_to do |format|
         format.html
         format.xls { send_data(@<%= plural_table_name %>.to_xls) }
+        format.json { render :json => @<%= plural_table_name %> }
       end
     end
 
@@ -87,6 +89,15 @@ module Admin
         admin_<%= index_helper %>_path(page: @current_page, search: @query),
         notice: actions_messages(<%= orm_class.build(class_name) %>)
       )
+    end
+    
+    def reload
+      @<%= plural_table_name %> = <%= class_name %>.order(:position)
+    end
+
+    def sort
+      <%= class_name %>.sorter(params[:row])
+      render :index
     end
 
     private
