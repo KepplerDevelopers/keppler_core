59727355769f46166fd5f2736575aa72d3ed1b7f
enmachs <enmachs@gmail.com>
2018-01-27 22:58:57 -0400

    Método sin uso eliminado - Icono cambiado y traducción agregada

--- lib/generators/keppler_scaffold/templates/controllers/controller.rb
+++ lib/generators/keppler_scaffold/templates/controllers/controller.rb
@@ -103,9 +103,7 @@ module Admin
     end
 
     private
-    def setting_search
-      
-    end
+    
     # Use callbacks to share common setup or constraints between actions.
     def set_<%= singular_table_name %>
       @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
