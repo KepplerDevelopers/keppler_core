6ad5b04c9f3bd78b96edc326cba3dab25cc32a03
enmachs <enmachs@gmail.com>
2018-02-06 21:16:14 -0400

    Detalles de drag, preview para imagen en listing, etc

--- app/assets/javascripts/admin/sidebar.js
+++ app/assets/javascripts/admin/sidebar.js
@@ -23,3 +23,21 @@ function moveDropdown(){
     $(this).css('top', 40 + (40 * idx) + "px")
   });
 }
+function renderImg(){
+  xOffset = -20;
+  yOffset = 40;
+  $('.show-row img').hover(function(e){
+    $( "body" ).append( "<p id='preview'><img src='"+ this.src +"' alt='Image preview' class='img-show' /></p>" );
+    $("#preview")
+      .css("top",(e.pageY - xOffset) + "px")
+      .css("left",(e.pageX + yOffset) + "px")
+      .fadeIn("slow");
+  }, function(){
+    $("#preview").remove();
+  });
+  $(".show-row img").mousemove(function(e){
+    $("#preview")
+      .css("top",(e.pageY - xOffset) + "px")
+      .css("left",(e.pageX + yOffset) + "px");
+  });
+}
