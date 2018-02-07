46737e68713ae31d4a838aeb822deaf12ccf525a
enmachs <enmachs@gmail.com>
2018-02-07 15:31:43 -0400

    Optimizada función dropdown.

--- app/assets/javascripts/admin/dropdown_top.js
+++ app/assets/javascripts/admin/dropdown_top.js
@@ -1,6 +1,12 @@
 function moveDropdown(){
-  var dropdown = $(".actions.dropdown-menu");
-  dropdown.each(function(idx){
-    $(this).css('top', 40 + (40 * idx) + "px")
+  // var dropdown = $(".actions.dropdown-menu");
+  // dropdown.each(function(idx){
+  //   $(this).css('top', 40 + (40 * idx) + "px")
+  // });
+  $('.action-btn').click(function(e){
+    var that = this;
+    let a = $(`ul[aria-labelledby='${that.id}']`)[0];
+    let b = $(that).offset().top;
+    $(a).css("top",(b - 122) + "px");
   });
 }
