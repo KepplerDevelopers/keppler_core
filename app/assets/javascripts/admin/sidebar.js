c4124472ff1c2c20d367ddab165e45d90aeb8266
enmachs <enmachs@gmail.com>
2018-02-07 10:27:19 -0400

    Lógica js separada, recursos no necesarios eliminados

--- app/assets/javascripts/admin/sidebar.js
+++ app/assets/javascripts/admin/sidebar.js
@@ -17,27 +17,3 @@ $(function ($) {
 
 
 });
-function moveDropdown(){
-  var dropdown = $(".actions.dropdown-menu");
-  dropdown.each(function(idx){
-    $(this).css('top', 40 + (40 * idx) + "px")
-  });
-}
-function renderImg(){
-  xOffset = -20;
-  yOffset = 40;
-  $('.show-row img').hover(function(e){
-    $( "body" ).append( "<p id='preview'><img src='"+ this.src +"' alt='Image preview' class='img-show' /></p>" );
-    $("#preview")
-      .css("top",(e.pageY - xOffset) + "px")
-      .css("left",(e.pageX + yOffset) + "px")
-      .fadeIn("slow");
-  }, function(){
-    $("#preview").remove();
-  });
-  $(".show-row img").mousemove(function(e){
-    $("#preview")
-      .css("top",(e.pageY - xOffset) + "px")
-      .css("left",(e.pageX + yOffset) + "px");
-  });
-}
