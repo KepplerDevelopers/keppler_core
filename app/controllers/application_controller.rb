6938d2410535cc093248be02fc1e6e5efc9d0457
Luis Perez <luis.prz7@gmail.com>
2018-03-26 17:29:20 -0400

    Implementado rutas con distintos idiomas

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
   layout :layout_by_resource
   before_filter :configure_permitted_parameters, if: :devise_controller?
   before_filter :appearance
   before_action :set_apparience_colors
+  skip_around_action :set_locale_from_url
   include PublicActivity::StoreController
   include AdminHelper
 
