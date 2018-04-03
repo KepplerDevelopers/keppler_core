bdda1ea7e5f9193d184ff62a01844840ec6da37f
pdrowr <pdrowr@gmail.com>
2018-04-02 23:51:15 -0300

    ADDED methods 'keppler_admin' and 'admin'

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -2,13 +2,56 @@
 class ApplicationPolicy
   attr_reader :user, :record
 
-  # def initialize(user, record)
-  #   @user = user
-  #   @record = record
-  # end
+  def index?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    keppler_admin? || admin?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    keppler_admin? || admin?
+  end
+
+  def clone?
+    keppler_admin? || admin?
+  end
+
+  def show?
+    true
+  end
+
+  def destroy_multiple?
+    destroy?
+  end
+
+  def destroy?
+    keppler_admin? || admin?
+  end
+
+  def import?
+    keppler_admin? || admin?
+  end
+
+  def keppler_admin?
+    user.keppler_admin?
+  end
 
   def admin?
-    user.keppler_admin? || user.admin?
+    user.admin?
+  end
+
+  def same_user?(id)
+    user.id.eql?(id)
   end
 
   def scope
