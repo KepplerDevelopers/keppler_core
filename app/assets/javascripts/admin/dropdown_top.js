009975ad8a320309b58a2e7f51e0acbd853811dc
anyelopetit <anyelopetit@gmail.com>
2018-04-04 09:39:04 -0400

    mejorado responsive en dropdown menu de cada registro

--- app/assets/javascripts/admin/dropdown_top.js
+++ app/assets/javascripts/admin/dropdown_top.js
@@ -7,6 +7,10 @@ function moveDropdown(){
     var that = this;
     let a = $(`ul[aria-labelledby='${that.id}']`)[0];
     let b = $(that).offset().top;
-    $(a).css("top",(b - 122) + "px");
+    if ($(document).width() > 767) {
+      $(a).css("top",(b - 122) + "px");
+    } else {
+      $(a).css("top",(b - 192) + "px");
+    }
   });
 }
