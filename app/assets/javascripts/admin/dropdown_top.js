fa94692f53eab47fb85b082026191d0b5f2e3b46
anyelopetit <anyelopetit@gmail.com>
2018-04-04 09:57:55 -0400

    deleted cancan trashes

--- app/assets/javascripts/admin/dropdown_top.js
+++ app/assets/javascripts/admin/dropdown_top.js
@@ -1,8 +1,4 @@
 function moveDropdown(){
-  // var dropdown = $(".actions.dropdown-menu");
-  // dropdown.each(function(idx){
-  //   $(this).css('top', 40 + (40 * idx) + "px")
-  // });
   $('.action-btn').click(function(e){
     var that = this;
     let a = $(`ul[aria-labelledby='${that.id}']`)[0];
