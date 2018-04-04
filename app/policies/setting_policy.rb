feae6474a3de2f7550be03704bf84f1c187e956f
anyelopetit <anyelopetit@gmail.com>
2018-04-04 15:54:58 -0400

    refactorizando codigo repetido de Pundit

--- app/policies/setting_policy.rb
+++ app/policies/setting_policy.rb
@@ -1,9 +1,3 @@
 # Policy for setting model
 class SettingPolicy < ControllerPolicy
-  attr_reader :user, :objects
-
-  def initialize(user, objects)
-    @user = user
-    @objects = objects
-  end
 end
