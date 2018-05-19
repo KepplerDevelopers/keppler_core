7e84c524e98fc79bd9378fc9558e4af242c239cf
anyelopetit <anyelopetit@gmail.com>
2018-05-19 12:05:50 -0400

    removed byebug in meta tags

---
+++ app/assets/javascripts/admin/swal.js
@@ -0,0 +1,37 @@
+$(function ($) {
+  $('.delete').click(function(e){
+    e.preventDefault()
+    swal({
+      title: 'Submit your Github username',
+      input: 'text',
+      inputAttributes: {
+        autocapitalize: 'off'
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Look up',
+      showLoaderOnConfirm: true,
+      preConfirm: (login) => {
+        return fetch(`//api.github.com/users/${login}`)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error(response.statusText)
+            }
+            return response.json()
+          })
+          .catch(error => {
+            swal.showValidationError(
+              `Request failed: ${error}`
+            )
+          })
+      },
+      allowOutsideClick: () => !swal.isLoading()
+    }).then((result) => {
+      if (result.value) {
+        swal({
+          title: `${result.value.login}'s avatar`,
+          imageUrl: result.value.avatar_url
+        })
+      }
+    })
+  })
+});
--- app/controllers/admin/meta_tags_controller.rb
+++ app/controllers/admin/meta_tags_controller.rb
@@ -78,7 +78,6 @@ module Admin
     end
 
     def upload
-      byebug
       MetaTag.upload(params[:file])
       redirect_to(
         admin_meta_tags_path(page: @current_page, search: @query),
