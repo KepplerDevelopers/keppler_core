190b6a52115015924ce0d0bfe8973f06d31277e2
Paola Vargas <vrgspaola68@gmail.com>
2018-05-24 17:11:50 -0400

    404-500 errors ui

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_18_185354) do
+ActiveRecord::Schema.define(version: 2017_12_25_023933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,16 +66,6 @@ ActiveRecord::Schema.define(version: 2018_05_18_185354) do
     t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "pruebas", force: :cascade do |t|
-    t.string "name"
-    t.string "image"
-    t.integer "position"
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["deleted_at"], name: "index_pruebas_on_deleted_at"
   end
 
   create_table "roles", force: :cascade do |t|
