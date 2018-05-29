be59e5ad347badc14dbdc4618f53adec9e139128
Anyelo Petit <anyelopetit@gmail.com>
2018-05-29 10:14:04 -0400

    Anyelopetit/181/social share (#187)
    Agregados meta tags para compartir título, imagen y descripción de las webs por Facebook, Whatsapp, Twitter  y demás redes sociales

--- app/controllers/app/app_controller.rb
+++ app/controllers/app/app_controller.rb
@@ -2,22 +2,45 @@ module App
   # AppController -> Controller out the back-office
   class AppController < ::ApplicationController
     layout 'app/layouts/application'
+    skip_before_action :verify_authenticity_token
+    before_action :set_locale
     before_action :set_metas
     before_action :set_analytics
 
     def set_metas
+      @theme_color = nil
+      # Descomentar el modelo que exista depende del proyecto
+      # @post = KepplerBlog::Post.find(params[:id])
+      # @product = Product.find(params[:id])
       @setting = Setting.first
-      @title = @setting.name
-      @description = @setting.description
-      @favicon = @setting.favicon
       @meta = MetaTag.get_by_url(request.url)
       @social = SocialAccount.last
+      @meta_title = MetaTag.title(@post, @product, @setting)
+      @meta_description = MetaTag.description(@post, @product, @setting)
+      @meta_image = MetaTag.image(request, @post, @product, @setting)
+      @meta_locale = @locale.eql?('es') ? 'es_VE' : 'en_US'
+      @meta_locale_alternate = @locale.eql?('es') ? 'en_US' : 'es_VE'
+      @country_code = @locale.eql?('es') ? 'VE' : 'US'
     end
 
     private
 
+    def set_locale
+      if params[:locale]
+        @locale = I18n.locale = params[:locale]
+      elsif request.env['HTTP_ACCEPT_LANGUAGE']
+        request_lang = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/)[0]
+        @locale = I18n.locale = request_lang.eql?('es') ? 'es' : 'en'
+      end
+    end
+
+    def default_url_options(options = {})
+      logger.debug "default_url_options is passed options: #{options.inspect}\n"
+      { locale: I18n.locale }
+    end
+
     def set_analytics
-      @scripts = Script.all
+      @scripts = Script.select { |x| x.url == request.env['PATH_INFO'] }
     end
   end
 end
