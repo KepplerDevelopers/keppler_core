039ddbcadd8723150feafadd479d1e7a63ce1ad1
anyelopetit <anyelopetit@gmail.com>
2018-05-30 17:35:43 -0400

    removed byebug_history and person test module

--- app/policies/person_policy.rb
+++
@@ -1,9 +0,0 @@
-# Policy for Person model
-class PersonPolicy < ControllerPolicy
-  attr_reader :user, :objects
-
-  def initialize(user, objects)
-    @user = user
-    @objects = objects
-  end
-end
