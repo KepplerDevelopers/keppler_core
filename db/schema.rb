0797f0f28de859bcc56bf62d56752702a239f0aa
Paola Vargas <vrgspaola68@gmail.com>
2018-05-30 12:45:38 -0400

    paolavrgs/10/errors ui (#192)
    * 404-500 errors ui
    
    * 404-500 vistas - astronaut added
    
    * 403
    
    * Responsive - Quitar efectos en resoluciones pequeñas
    
    * Not authorized 403 view
    
    * Responsive done - 422 created

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2018_05_03_174351) do
     t.datetime "updated_at", null: false
     t.index ["deleted_at"], name: "index_pruebas_on_deleted_at"
   end
-
+  
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
