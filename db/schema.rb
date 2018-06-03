6488a09ac96b876f38219a3c81a7b1118fef601d
anyelopetit <anyelopetit@gmail.com>
2018-06-03 03:14:41 -0400

    arreglados estilos del custom keppler switch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_03_023439) do
+ActiveRecord::Schema.define(version: 2018_06_03_070548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,28 @@ ActiveRecord::Schema.define(version: 2018_06_03_023439) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "photo"
+    t.string "email"
+    t.string "phone"
+    t.integer "age"
+    t.float "weight"
+    t.date "birth"
+    t.time "hour"
+    t.bigint "user_id"
+    t.boolean "public"
+    t.datetime "arrived"
+    t.decimal "decimal"
+    t.integer "position"
+    t.datetime "deleted_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["deleted_at"], name: "index_people_on_deleted_at"
+    t.index ["user_id"], name: "index_people_on_user_id"
+  end
+
   create_table "permissions", force: :cascade do |t|
     t.jsonb "modules"
     t.bigint "role_id"
@@ -88,15 +110,6 @@ ActiveRecord::Schema.define(version: 2018_06_03_023439) do
     t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
     t.index ["name"], name: "index_roles_on_name"
     t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
-  end
-
-  create_table "rooms", force: :cascade do |t|
-    t.string "name"
-    t.integer "position"
-    t.datetime "deleted_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["deleted_at"], name: "index_rooms_on_deleted_at"
   end
 
   create_table "scaffolds", force: :cascade do |t|
@@ -190,5 +203,6 @@ ActiveRecord::Schema.define(version: 2018_06_03_023439) do
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
+  add_foreign_key "people", "users"
   add_foreign_key "permissions", "roles"
 end
