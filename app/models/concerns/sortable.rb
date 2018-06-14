4be93f46db437cc47e11bce39d4263a4829d0e32
Anyelo Petit <anyelopetit@gmail.com>
2018-06-14 17:10:48 -0400

    Anyelopetit/243/css linter (#256)
    Aplicando SCSS Linters

--- app/models/concerns/sortable.rb
+++ app/models/concerns/sortable.rb
@@ -5,9 +5,7 @@ module Sortable
   extend ActiveSupport::Concern
   included do
     def self.sorter(params)
-      params.each_with_index do |id, idx|
-        find(id).update(position: idx.to_i + 1)
-      end
+      params.each_with_index { |i, id| find(i).update(position: id.to_i + 1) }
     end
   end
 end
