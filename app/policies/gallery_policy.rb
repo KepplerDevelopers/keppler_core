6104b18e2c948ac0737fc2cb6c0308c4e27f2433
anyelopetit <anyelopetit@gmail.com>
2018-06-15 09:09:11 -0400

    solucionadas ofensas de CI

--- app/policies/gallery_policy.rb
+++
@@ -1,9 +0,0 @@
-# Policy for Gallery model
-class GalleryPolicy < ControllerPolicy
-  attr_reader :user, :objects
-
-  def initialize(user, objects)
-    @user = user
-    @objects = objects
-  end
-end
