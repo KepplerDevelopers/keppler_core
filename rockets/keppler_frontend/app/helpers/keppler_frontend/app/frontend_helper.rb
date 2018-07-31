ca1cf1bbbe877e0d68695e33f1ea16cff16eaf31
luprz <luis.prz7@gmail.com>
2018-07-31 16:07:10 -0400

    sistema de temas listo

--- rockets/keppler_frontend/app/helpers/keppler_frontend/app/frontend_helper.rb
+++ rockets/keppler_frontend/app/helpers/keppler_frontend/app/frontend_helper.rb
@@ -1,4 +1,7 @@
 module KepplerFrontend
-  module App::FrontendHelper
+  module App::FrontendHelper    
+    def set_head
+      render 'layouts/keppler_frontend/app/layouts/head'
+    end
   end
 end
