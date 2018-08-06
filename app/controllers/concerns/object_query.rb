63fd2911cc79cea230684d61f44fb091a5136943
Anyelo Petit <anyelopetit@gmail.com>
2018-08-06 09:46:38 -0400

    Anyelopetit/249/load data (#311)
    Solved issues in upload and download data by CSV and XLS

--- app/controllers/concerns/object_query.rb
+++ app/controllers/concerns/object_query.rb
@@ -14,11 +14,24 @@ module ObjectQuery
     !objects.first_page? && objects.size.zero?
   end
 
+  def send_format_data(objects, extension)
+    models = objects.model.to_s.downcase.pluralize
+    t_models = t("keppler.models.pluralize.#{models}").humanize
+    filename = "#{t_models} - #{I18n.l(Time.now, format: :short)}"
+    objects_array = objects.order(:created_at)
+    case extension
+    when 'csv'
+      send_data objects_array.to_csv, filename: "#{filename}.csv"
+    when 'xls'
+      send_data objects_array.to_a.to_xls, filename: "#{filename}.xls"
+    end
+  end
+
   def respond_to_formats(objects)
     respond_to do |format|
       format.html
-      format.csv { send_data objects.to_csv }
-      format.xls { send_data objects.to_xls }
+      format.csv { send_format_data(objects, 'csv') }
+      format.xls { send_format_data(objects, 'xls') }
       format.json { render json: objects }
     end
   end
