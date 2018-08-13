2676f15648fdfab4fb91be520d7d02aac215a30e
pdrowr <pdrowr@gmail.com>
2018-08-12 21:37:24 -0300

    fixed bug in code save function

--- rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/admin/partial_editor.js
+++ rockets/keppler_frontend/app/assets/javascripts/keppler_frontend/admin/partial_editor.js
@@ -162,3 +162,13 @@ var codePartialJs = {
     }
   }
 }
+
+
+function copyHtml(id) {
+  const el = document.createElement('textarea');
+  el.value = $("#"+id)[0].attributes.value.value;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
