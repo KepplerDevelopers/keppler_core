4072abdab638519cb3c7ef38c56abdb73466585c
Anyelo Petit <anyelopetit@gmail.com>
2018-08-25 17:18:12 -0400

    added sweetalert2 to notifications

--- app/assets/javascripts/admin/notification.js
+++ app/assets/javascripts/admin/notification.js
@@ -1,24 +1,14 @@
 function notify(notice){
-  new Noty({
-    text: notice,
-    layout: 'bottomCenter',
-    theme: 'relax', // mint, sunset, relax, nest, metroui, semanticui, light, bootstrap-v3, bootstrap-v4
-    closeWith: ['click', 'button'], // ['click', 'button', 'hover', 'backdrop'] // backdrop click will close all notifications
-    timeout: 2000, // [integer|boolean] delay for closing event in milliseconds. Set false for sticky notifications
-    type: 'notification', // success, error, warning, information, notification
-    // modal: true, // [boolean] if true adds an overlay
-    killer: true, // [boolean] if true closes all notifications and shows itself
-    progressBar: true, // default true
-    animation: {
-      open: 'animated bounceIn', // Animate.css class names
-      close: 'animated flipOutX', // Animate.css class names
-      easing: 'swing',
-      speed: 500 // opening & closing animation speed
-    }
-  }).show();
-}
+  const toast = swal.mixin({
+    toast: true,
+    position: 'bottom',
+    showConfirmButton: false,
+    timer: 3000,
+    grow: 'fullscreen'
+  });
 
-$(document).on('confirm:complete', function(e, answer) {
-  $(".spinner").css('display', 'block');
-  return answer;
-})
+  toast({
+    type: 'success',
+    title: notice
+  })
+}
