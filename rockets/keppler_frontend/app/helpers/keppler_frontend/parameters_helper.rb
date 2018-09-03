4a7ab8d68f1bda3971ca7567c6c70643ceb90b96
Pedro Guerra <pdrowr@gmail.com>
2018-09-03 12:00:10 -0400

    Frontend Functions (#375)
    Create controller functions in Keppler

---
+++ rockets/keppler_frontend/app/helpers/keppler_frontend/parameters_helper.rb
@@ -0,0 +1,4 @@
+module KepplerFrontend
+  module ParametersHelper
+  end
+end
---
+++ rockets/keppler_frontend/test/models/keppler_frontend/function_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+module KepplerFrontend
+  class FunctionTest < ActiveSupport::TestCase
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
