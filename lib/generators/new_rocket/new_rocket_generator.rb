d0d69afe3ed2dc6dd25f19e079151a33d3c4e03b
Anyelo Petit <anyelopetit@gmail.com>
2018-09-10 12:00:07 -0400

    fixed rockets head path

--- lib/generators/new_rocket/new_rocket_generator.rb
+++ lib/generators/new_rocket/new_rocket_generator.rb
@@ -2,8 +2,8 @@ class NewRocketGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
 
   def create_rocket
-    rocket_name = file_name.split('keppler_').last
-    rocket_directory = "#{Rails.root}/rockets/keppler_#{rocket_name}"
+    rocket_name = "keppler_#{file_name.split('keppler_').last}"
+    rocket_directory = "#{Rails.root}/rockets/#{rocket_name}"
     if Dir.exists? rocket_directory
       say "\n... Aborting. This Rocket already exists ...\n", :red
     else
@@ -11,7 +11,7 @@ class NewRocketGenerator < Rails::Generators::NamedBase
       copy_generator(rocket_name)
       copy_layouts(rocket_name)
       add_helper_in_application_core
-      add_head_in_application_rocket(rocket_directory)
+      add_head_in_application_rocket(rocket_name, rocket_directory)
       create_policies_folder(rocket_name)
       say "\n=== All Done. #{class_name} Rocket has been created and installed ===\n", :green
     end
@@ -22,12 +22,12 @@ class NewRocketGenerator < Rails::Generators::NamedBase
   def generate_rocket(rocket_name)
     say "\n*** Generating rocket ***\n"
 
-    system("rails plugin new rockets/keppler_#{rocket_name} --mountable")
+    system("rails plugin new rockets/#{rocket_name} --mountable")
     say "=== #{class_name} Rocket has been generated ===\n", :green
 
     say "\n*** Replacing 'TODO' by 'https://keppleradmin.com' in #{class_name} gemspec ***\n"
 
-    gemspec = "rockets/keppler_#{rocket_name}/keppler_#{rocket_name}.gemspec"
+    gemspec = "rockets/#{rocket_name}/#{rocket_name}.gemspec"
     path = File.join(Rails.root, gemspec)
     content = File.read(path).gsub(/TODO/, 'https://keppleradmin.com')
     if File.open(path, 'wb') { |file| file.write(content) }
@@ -40,7 +40,7 @@ class NewRocketGenerator < Rails::Generators::NamedBase
   def copy_generator(rocket_name)
     say "\n*** Copying generators from Core to Rocket ***\n"
 
-    if directory "#{Rails.root}/lib/plugins/generators", "#{Rails.root}/rockets/keppler_#{rocket_name}/lib/generators"
+    if directory "#{Rails.root}/lib/plugins/generators", "#{Rails.root}/rockets/#{rocket_name}/lib/generators"
       say "=== Generators has been copied from Core to #{class_name} Rocket ===/n", :green
     else
       say "!!! Generators could not be copied. Please check if /lib/plugins exists !!!/n", :red
@@ -50,19 +50,19 @@ class NewRocketGenerator < Rails::Generators::NamedBase
   def copy_layouts(rocket_name)
     say "\n*** Copying Layouts from Core to Rocket ***\n"
 
-    if FileUtils.mkdir_p "#{Rails.root}/rockets/keppler_#{rocket_name}/app/views/keppler_#{rocket_name}/admin/layouts"
+    if FileUtils.mkdir_p "#{Rails.root}/rockets/#{rocket_name}/app/views/#{rocket_name}/admin/layouts"
       say "=== #{class_name} Layouts folder has been created ===\n", :green
     else
-      say  "!!! #{class_name} Layouts folder could not be created. Please check if /rockets/keppler_#{rocket_name} exists !!!\n", :red
+      say  "!!! #{class_name} Layouts folder could not be created. Please check if /rockets/#{rocket_name} exists !!!\n", :red
     end
 
-    if directory "#{Rails.root}/lib/plugins/layouts", "#{Rails.root}/rockets/keppler_#{rocket_name}/app/views/keppler_#{rocket_name}/admin/layouts"
+    if directory "#{Rails.root}/lib/plugins/layouts", "#{Rails.root}/rockets/#{rocket_name}/app/views/#{rocket_name}/admin/layouts"
       say "=== Layouts has been copied from Core to #{class_name} Rocket ===/n", :green
     else
       say "!!! Layouts could not be copied. Please check if /lib/plugins exists !!!/n", :red
     end
 
-    system("ruby #{Rails.root}/lib/plugins/install.rb keppler_#{rocket_name} #{Rails.root}")
+    system("ruby #{Rails.root}/lib/plugins/install.rb #{rocket_name} #{Rails.root}")
   end
 
   def add_helper_in_application_core
@@ -73,28 +73,28 @@ class NewRocketGenerator < Rails::Generators::NamedBase
     say "=== #{class_name}'s helpers has been copied ===", :green
   end
 
-  def add_head_in_application_rocket(rocket_directory)
-    head = "#{rocket_directory}/app/views/keppler_#{file_name}/admin/layouts/_head.haml"
+  def add_head_in_application_rocket(rocket_name, rocket_directory)
+    head = "#{rocket_directory}/app/views/#{rocket_name}/admin/layouts/_head.haml"
     say "\n*** Creating #{class_name}'s head ***"
     FileUtils.touch head
     File.open(head, "w+") do |f|
-      f.write("// #{class_name} Stylesheets\n= stylesheet_link_tag '#{file_name}/admin/application', media: 'all', 'data-turbolinks-track': true\n// #{class_name} Javascripts\n= javascript_include_tag '#{file_name}/admin/application', 'data-turbolinks-track': true")
+      f.write("// #{class_name} Stylesheets\n= stylesheet_link_tag '#{rocket_name}/application', media: 'all', 'data-turbolinks-track': true\n// #{class_name} Javascripts\n= javascript_include_tag '#{rocket_name}/application', 'data-turbolinks-track': true")
     end
     say "=== #{class_name}'s head has been created ===", :green
     say "\n*** Adding #{class_name}'s head in its application.haml ***"
-    inject_into_file "#{rocket_directory}/app/views/keppler_#{file_name}/admin/layouts/application.html.haml", after: "= render 'admin/layouts/head'" do
-      "\n  = render '#{file_name}/admin/layouts/head'"
+    inject_into_file "#{rocket_directory}/app/views/#{rocket_name}/admin/layouts/application.html.haml", after: "= render 'admin/layouts/head'" do
+      "\n  = render '#{rocket_name}/admin/layouts/head'"
     end
     say "=== #{class_name}'s head has been added ===", :green
   end
 
   def create_policies_folder(rocket_name)
-    say "\n*** Creating Policies folder in /rockets/#{file_name}/app/policies ***\n"
+    say "\n*** Creating Policies folder in /rockets/#{rocket_name}/app/policies ***\n"
 
-    if FileUtils.mkdir_p "#{Rails.root}/rockets/keppler_#{rocket_name}/app/policies"
+    if FileUtils.mkdir_p "#{Rails.root}/rockets/#{rocket_name}/app/policies"
       say "=== #{class_name} Policies folder has been created ===\n", :green
     else
-      say  "!!! #{class_name} Policies folder could not be created. Please check if /rockets/keppler_#{rocket_name} exists !!!\n", :red
+      say  "!!! #{class_name} Policies folder could not be created. Please check if /rockets/#{rocket_name} exists !!!\n", :red
     end
   end
 end
