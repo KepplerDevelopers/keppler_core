cb95e88f5250c4f99a254fa8f298cbfcc892f091
pdrowr <pdrowr@gmail.com>
2018-09-11 10:29:41 -0400

    change default arrow

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -2,6 +2,6 @@ Ransack.configure do |c|
   c.custom_arrows = {
     up_arrow: '<i class="icon-arrow-up"></i>',
     down_arrow: '<i class="icon-arrow-down"></i>',
-    default_arrow: '<i class="icon-arrow-down"></i>'
+    default_arrow: '<i class="icon-arrow-up"></i>'
   }
 end
