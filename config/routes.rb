8ac5407086cf16946fd070121a3bed8247b3d443
pdrowr <pdrowr@gmail.com>
2018-10-05 12:43:18 -0400

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     get '/index', to: 'app/front#index', as: :app_index
   end
 
+  # root to: 'app/front#index'
+
   devise_for :users, skip: KepplerConfiguration.skip_module_devise
   post '/filter', to: 'admin/users#filter_by_role', as: :filter_by_role
 
