8b654a0a31a711e8ec4cc1e3ad52ac88a8c5e611
pdrowr <pdrowr@gmail.com>
2018-10-09 12:36:46 -0400

    solved permissions bugs

---
+++ app/policies/permission_policy.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+# Policy for Permission model
+class PermissionPolicy < ControllerPolicy
+  attr_reader :user, :objects
+
+  def initialize(user, objects)
+    @user = user
+    @objects = objects
+  end
+
+  def clone?
+    false
+  end
+
+  def add?
+    true
+  end
+end
