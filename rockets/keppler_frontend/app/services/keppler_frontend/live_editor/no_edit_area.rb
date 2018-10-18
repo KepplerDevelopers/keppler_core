a53205f1cc9bbf9f05bf94ff96726bfdf029aeff
luprz <luis.prz7@gmail.com>
2018-10-18 12:55:33 -0400

    Creado y testeado el service para areas no editables

--- rockets/keppler_frontend/app/services/keppler_frontend/live_editor/no_edit_area.rb
+++ rockets/keppler_frontend/app/services/keppler_frontend/live_editor/no_edit_area.rb
@@ -4,15 +4,64 @@ module KepplerFrontend
   module LiveEditor
     # CssHandler
     class NoEditArea
-      def initialize; end
+      def initialize(html_lines)
+        @html_original = File.readlines(html_lines)
+      end
 
-      def ids(section)
-        doc = Nokogiri::HTML(section)
+      def ids
+        doc = Nokogiri::HTML(@html_original.join)
         nodes = []
         doc.css('keppler-no-edit').each do |link|
           nodes << [link.attribute('id').value]
         end
         nodes
+      end
+
+      def code_no_edit(ids)
+        ids.each_with_index do |id, idx|
+          idx_section = search_area(@html_original, id.first)
+          ids[idx] << @html_original[ idx_section[0]..idx_section[1] ]
+        end
+      end
+
+      def merge_to(html_editor)
+        html_editor = File.readlines(html_editor)
+        code_no_edit(ids).each do |code|
+          next if delete_empty_areas?(html_editor, code)
+          idx_section = search_area(html_editor, code.first)
+          html_editor = fusion(html_editor, code, idx_section)
+        end
+        html_editor
+      end
+
+      private
+
+      def code_search(html)
+        KepplerFrontend::Utils::CodeSearch.new(html)
+      end
+
+      def search_area(html, id)
+        find = code_search(html)
+        find.search_section("id='#{id}'", '</keppler-no-edit>')
+      end
+
+      def delete_empty_areas?(html_editor, code)
+        find = code_search(html_editor)
+        line = "id=\"#{code.first}\"></keppler-no-edit>"
+        idx_empty = find.search_line(line)
+        !idx_empty.zero? ? html_editor.slice!(idx_empty) : false
+      end
+
+      def fusion(html_editor, code, idx_section)
+        idx_begin = idx_section[0]
+        idx_end = idx_section[1]
+        html_editor.slice!(idx_begin + 1..idx_end - 1)
+        return html_editor if idx_begin.zero?
+        code_last = code.last
+        code_last[1..code_last.length - 2].reverse.each do |line|
+          html_editor.insert(idx_begin + 1, "#{line}\n")
+        end
+        html_editor
       end
     end
   end
--- spec/services/keppler_frontend/live_editor/no_edit_area_spec.rb
+++ spec/services/keppler_frontend/live_editor/no_edit_area_spec.rb
@@ -4,8 +4,41 @@ require 'byebug'
 RSpec.describe KepplerFrontend::LiveEditor::NoEditArea, type: :services do
 
   context 'no edit area' do
-    context 'ids' do
-       
+    let(:fixture) { "#{Rails.root}/spec/fixtures/keppler_frontend/live_editor/" }
+
+    let(:file_original) { "#{fixture}/test_index_original.html" }
+
+    let(:file_editor) { "#{fixture}/test_index_editor.html" }
+
+    before(:each) do
+      @no_edit_area = KepplerFrontend::LiveEditor::NoEditArea.new(file_original)
+    end
+
+    context 'get no edit area ids' do
+      let(:no_edit_areas) { @no_edit_area.ids }
+
+      it { expect(no_edit_areas).to eq([["area_one"], ["area_two"], ["area_three"]]) }
+    end
+
+    context 'get no edit areas' do
+      let(:no_edit_areas) do
+        ids = @no_edit_area.ids
+        @no_edit_area.code_no_edit(ids)
+      end
+
+      it { expect(no_edit_areas).to be_a(Array) }
+      it { expect(no_edit_areas.count).to eq(3) }
+      it { expect(no_edit_areas[0]).to eq(["area_one", ["  <keppler-no-edit id='area_one'>\n", "    <%= 'area_one' %>\n", "  </keppler-no-edit>\n"]]) }
+      it { expect(no_edit_areas[1]).to eq(["area_two", ["  <keppler-no-edit id='area_two'>\n", "    <%= 'area_two' %>\n", "  </keppler-no-edit>\n"]]) }
+      it { expect(no_edit_areas[2]).to eq(["area_three", ["  <keppler-no-edit id='area_three'>\n", "    <%= 'area_three' %>\n", "  </keppler-no-edit>\n"]]) }
+    end
+
+    context 'merge no edit areas' do
+      let(:merge_no_edit_areas) do
+        @no_edit_area.merge_to(file_editor)
+      end
+
+      it { expect(merge_no_edit_areas).to eq(["<keppler-header>\n", "  <h1>Header</h1>\n", "  <keppler-no-edit id='area_one'>\n", "    <%= 'area_one' %>\n\n", "  </keppler-no-edit>\n", "</keppler-header>\n", "<keppler-view ='test_index'>\n", "  <h1>Test Index</h1>\n", "  <keppler-no-edit id='area_two'>\n", "    <%= 'area_two' %>\n\n", "  </keppler-no-edit>\n", "</keppler-view>\n", "<keppler-footer>\n", "  <h1>Footer</h1>\n", "  <keppler-no-edit id='area_three'>\n", "    <%= 'area_three' %>\n\n", "  </keppler-no-edit>\n", "</keppler-footer>"]) }
     end
   end
 end
