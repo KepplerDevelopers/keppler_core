1d97b2c681dcfacc99d151f968dc0c58e336f46f
luprz <luis.prz7@gmail.com>
2018-11-05 13:48:44 -0400

    cambios

--- rockets/keppler_frontend/app/controllers/keppler_frontend/app/frontend_controller.rb
+++ rockets/keppler_frontend/app/controllers/keppler_frontend/app/frontend_controller.rb
@@ -2,6 +2,7 @@ require_dependency "keppler_frontend/application_controller"
 
 module KepplerFrontend
   class App::FrontendController < App::AppController
+    # Begin callbacks area (don't delete)
     # End callbacks area (don't delete)
     include FrontsHelper
     layout 'layouts/keppler_frontend/app/layouts/application'
