4fc7c2f7835c47fb6fc92b6e22d2afd21ef9ef6a
pdrowr <pdrowr@gmail.com>
2018-11-05 16:22:46 -0400

    solved html bug in partials

--- rockets/keppler_frontend/app/models/keppler_frontend/concerns/partials/html_file.rb
+++ rockets/keppler_frontend/app/models/keppler_frontend/concerns/partials/html_file.rb
@@ -15,11 +15,11 @@ module KepplerFrontend
           begin_idx = 0
           end_idx = 0
           index_html.each do |idx|
-            begin_idx = index_html.find_index(idx) if idx.include?("<!-- begin #{name} -->\n")
-            end_idx = index_html.find_index(idx) if idx.include?("<!-- end #{name} -->\n")
+            begin_idx = index_html.find_index(idx) if idx.include?("<partial id='#{name}'>\n")
+            end_idx = index_html.find_index(idx) if idx.include?("</partial>\n")
           end
           # return if begin_idx==0
-          index_html = index_html[begin_idx+2..end_idx-2]
+          index_html = index_html[begin_idx+1..end_idx-1]
           index_html = index_html.map { |line| line[0, line.length] }
           index_html.join('')
         end
@@ -28,11 +28,9 @@ module KepplerFrontend
           file = File.open("#{url_front}/app/views/keppler_frontend/app/partials/#{underscore_name}.html.erb", "w")
           index_html = File.readlines(file)
           head_idx = 0
-          index_html.insert(head_idx.to_i + 1, "<!-- begin #{name} -->\n")
-          index_html.insert(head_idx.to_i + 2, "<div id='#{name}'>\n")
-          index_html.insert(head_idx.to_i + 3, "  <h1> #{name} partial </h1>")
-          index_html.insert(head_idx.to_i + 4, "\n</div>\n")
-          index_html.insert(head_idx.to_i + 5, "<!-- end #{name} -->\n")
+          index_html.insert(head_idx.to_i + 1, "<partial id='#{name}'>\n")
+          index_html.insert(head_idx.to_i + 2, "  <h1> #{name} partial </h1>")
+          index_html.insert(head_idx.to_i + 3, "\n</partial>\n")
           index_html = index_html.join('')
           File.write(file, index_html)
           file.close
@@ -52,13 +50,12 @@ module KepplerFrontend
           end_idx = 0
           default_idx = 0
           index_html.each do |idx|
-            begin_idx = index_html.find_index(idx) if idx.include?("<!-- begin #{name} -->\n")
-            end_idx = index_html.find_index(idx) if idx.include?("<!-- end #{name} -->\n")
+            begin_idx = index_html.find_index(idx) if idx.include?("<partial id='#{name}'>\n")
+            end_idx = index_html.find_index(idx) if idx.include?("</partial>\n")
             default_idx = index_html.find_index(idx) if idx.include?("  <h1> #{name} partial </h1>")
           end
-          index_html[begin_idx] = "<!-- begin #{html[:name]} -->\n"
-          index_html[begin_idx+1] = "<div id='#{html[:name]}'>\n"
-          index_html[end_idx] = "<!-- end #{html[:name]} -->\n"
+          index_html[begin_idx] = "<partial id='#{html[:name]}'>\n"
+          index_html[end_idx] = "</partial>"
           index_html[default_idx] = "  <h1> #{html[:name]} partial </h1>\n" unless default_idx.eql?(0)
           index_html = index_html.join('')
           File.write(file, index_html)
@@ -77,13 +74,13 @@ module KepplerFrontend
           begin_idx = 0
           end_idx = 0
           code_html.each do |i|
-            begin_idx = code_html.find_index(i) if i.include?("<!-- begin #{name} -->\n")
-            end_idx = code_html.find_index(i) if i.include?("<!-- end #{name} -->\n")
+            begin_idx = code_html.find_index(i) if i.include?("<partial id='#{name}'>\n")
+            end_idx = code_html.find_index(i) if i.include?("</partial>\n")
           end
           # return if begin_idx==0
-          code_html.slice!(begin_idx+2..end_idx-2)
+          code_html.slice!(begin_idx+1..end_idx-1)
           code.split("\n").each_with_index do |line, i|
-            code_html.insert(begin_idx+(i+2), "  #{line}\n")
+            code_html.insert(begin_idx+(i+1), "  #{line}\n")
           end
           code_html = code_html.join('')
           File.write(file, code_html)
