d66b76669ab9437303b7d49797e25ad0d8f800cf
luprz <luis.prz7@gmail.com>
2018-11-26 10:38:50 -0400

    Integrado ramas de git

--- app/services/admin/git_handler.rb
+++ app/services/admin/git_handler.rb
@@ -3,26 +3,25 @@
 module Admin
   # Git Handler
   class GitHandler
-
     def installed?
       @git_branch_name = `git status`
-      return true
+      true
     rescue StandardError
-      return false
+      false
     end
 
     def current_branch
       `git rev-parse --abbrev-ref HEAD`
     rescue StandardError
-      return false
+      false
     end
 
     def changes_count
-      status = `git diff --stat HEAD`
-      status_color = status.blank? ? '#12c752' : '#f98105'
-      { status: status.split("\n").last, color: status_color }
+      changes = `git diff --stat HEAD`
+      status_color = changes.blank? ? '#12c752' : '#f98105'
+      { changes: changes.split("\n").last, color: status_color }
     rescue StandardError
-      return false
+      false
     end
   end
 end
