2de683ae465a2a058ceccf906c94574394303bda
Luis Perez <luis.prz7@gmail.com>
2019-04-23 14:13:03 -0400

    Change login url (#568)
    * Change login url
    
    * Change login url
    
    * Change login url
    
    * Change login url
    
    * Create config section and time zone input
    
    * Create config section and time zone input
    
    * Create config section and time zone input
    
    * Configuration view refactor
    
    * Configuration view refactor
    
    * Somethigs rspecs
    
    * Change session controller to keppler session controller

--- config/initializers/keppler_configuration.rb
+++ config/initializers/keppler_configuration.rb
@@ -3,7 +3,7 @@ require "#{Rails.root}/lib/keppler_configuration.rb"
 KepplerConfiguration.setup do |config|
   # Allows you to choose the devise modules you want to skip
   #   example: [:registrations, :confirmations]
-  config.skip_module_devise = [:registrations]
+  config.skip_module_devise = [:registrations, :sessions]
   # Allows you to choose the number of rows to show per page
   config.default_per_page = 10
 end
