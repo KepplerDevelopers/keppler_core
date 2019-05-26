a3b14cbfc61695fb538685b6caa04eb4bd293fd2
luprz <luis.prz7@gmail.com>
2019-05-26 14:39:30 -0400

    Redirect to admin after login

--- app/controllers/devise/keppler_sessions_controller.rb
+++ app/controllers/devise/keppler_sessions_controller.rb
@@ -26,7 +26,7 @@ module Devise
       set_flash_message(:notice, :signed_in) if is_flashing_format?
       sign_in(resource_name, resource)
       yield resource if block_given?
-      respond_with resource, location: "/admin"
+      respond_with resource, location: admin_root_path
     end
 
     # DELETE /resource/sign_out
