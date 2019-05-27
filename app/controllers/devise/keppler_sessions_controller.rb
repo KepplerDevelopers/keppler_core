bfaa15c1a10ef731fefea5a6a4da6077277713b0
Luis Perez <luis.prz7@gmail.com>
2019-05-27 09:23:27 -0500

    Redirect to admin after user login (#584)
    * Gems cache
    
    * Redirect to admin after login
    
    * Redirect to admin after login

--- app/controllers/devise/keppler_sessions_controller.rb
+++ app/controllers/devise/keppler_sessions_controller.rb
@@ -26,7 +26,7 @@ module Devise
       set_flash_message(:notice, :signed_in) if is_flashing_format?
       sign_in(resource_name, resource)
       yield resource if block_given?
-      respond_with resource, location: after_sign_in_path_for(resource)
+      respond_with resource, location: admin_root_path
     end
 
     # DELETE /resource/sign_out
